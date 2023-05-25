97d6b1a5294534274f020a02c2e0f04f2205481a
Kevin Rivas <kevinsavir20@gmail.com>
2023-05-25 18:49:46 -0400

    se agregan animaciones y texto, se documenta acciones en el archivo index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 //Animacion boton carta Destacados
 $(document).ready(function(){
-    $(".move").click(function(){
+    $(".move").dblclick(function(){
         $(".carta1").animate({
             height: "toggle"
         });
@@ -8,7 +8,7 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-    $(".move").click(function(){
+    $(".move").dblclick(function(){
         $(".carta2").animate({
             height: "toggle"
         });
@@ -16,7 +16,7 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-    $(".move").click(function(){
+    $(".move").dblclick(function(){
         $(".carta3").animate({
             height: "toggle"
         });
@@ -24,9 +24,32 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-    $(".move").click(function(){
+    $(".move").dblclick(function(){
         $(".carta4").animate({
             height: "toggle"
         });
     });
 });
+
+
+//Alerta con la entrada del mouse
+$(document).ready(function(){
+    $("#viajesChile").mouseenter(function(){
+        alert("Bienvenido a Viajes Chile");
+    });
+});
+
+//Animacion boton para enviar formulario y alerta de envio correcto
+$(document).ready(function(){
+    $("#presentacion").click(function(){
+            $(this).hide();
+        });
+    });
+
+//Mensaje de mensaje enviado correctamente
+    $(document).ready(function(){
+        $(".submit").click(function(){
+            alert("Su informacion ha sido enviada correctamente");
+            });
+        });
+    
