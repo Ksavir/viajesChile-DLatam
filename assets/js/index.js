f36ae8ded1b61e88beedbda4772dea17358dcb7b
Kevin Rivas <kevinsavir20@gmail.com>
2023-05-25 18:26:09 -0400

    se agregar boton con animacion jquery para la seccion destacados

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+//Animacion boton carta Destacados
+$(document).ready(function(){
+    $(".move").click(function(){
+        $(".carta1").animate({
+            height: "toggle"
+        });
+    });
+});
+
+$(document).ready(function(){
+    $(".move").click(function(){
+        $(".carta2").animate({
+            height: "toggle"
+        });
+    });
+});
+
+$(document).ready(function(){
+    $(".move").click(function(){
+        $(".carta3").animate({
+            height: "toggle"
+        });
+    });
+});
+
+$(document).ready(function(){
+    $(".move").click(function(){
+        $(".carta4").animate({
+            height: "toggle"
+        });
+    });
+});
